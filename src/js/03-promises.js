2c5021b81036744e7c63c1cb638418815338b2eb
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-01-11 11:56:11 +0200

    previous solution

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,26 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const refs = {
+//   formEL: document.querySelector('form'),
+// } 
+// refs.formEL.addEventListener('submit', event=>{
+//   event.preventDefault();
+
+// const delay = event.target.elements.delay.value;
+// const count = event.target.elements.amount.value;
+// const interval = event.target.elements.step.value;
+
+// setTimeout(()=>{
+//   for (let i=0; i<count; i++){
+// setTimeout(callback, interval * i, interval* i + +delay)
+//   }
+// },delay);
+
+// });
+// function callback (data){
+//   console.log(data);
+// }
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +29,12 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
