718ae2700e5f6451da96f244955b99c757473c70
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-01-12 16:03:03 +0200

    fixed notifix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-// import { Notify } from "notiflix/build/notiflix-notify-aio";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const refs = {
   form: document.querySelector('.form'),
@@ -31,11 +31,11 @@ function onSubmitFct(e) {
   for (let promiseID = 0; promiseID < amount; promiseID++) {
 
     createPromise(promiseID + 1, delay).then(({ position, delay }) => {
-      // Notify.success 
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      //
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
     }).catch(({ position, delay }) => {
-      // Notify.failure
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      // 
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
     delay = delay + step
   }
@@ -44,4 +44,4 @@ function onSubmitFct(e) {
 refs.form.addEventListener('submit', onSubmitFct);
 
 
-
+console.log()
